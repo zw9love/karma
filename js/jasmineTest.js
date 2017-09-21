7d061dc26f0aa7579ed3b816a02e2ab7937b65e0
zw9love <18514075699@163.com>
2017-09-21 16:14:34 +0800

    karma test

---
+++ js/jasmineTest.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zengwei on 2017/9/21
+ */
+describe("A suite of basic functions", function () {
+    it("test", function () {
+        expect("abcd").toEqual(TT());
+    });
+});
---
+++ js/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zengwei on 2017/9/21
+ */
+
+function TT() {
+    return "abc";
+}
